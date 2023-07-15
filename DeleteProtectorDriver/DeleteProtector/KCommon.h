389bd3edb2efbb50f0a827b39c15a59522359b5b
Y3A <62646606+Y3A@users.noreply.github.com>
2023-07-15 13:18:04 +0800

    Delete DeleteProtectorDriver directory

--- DeleteProtectorDriver/DeleteProtector/KCommon.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define DRIVER_TAG      'tcpK'
-#define DRIVER_PREFIX   "KProtector: "
-
-#define err(msg, status) KdPrint((DRIVER_PREFIX "Error %s : (0x%08X)\n", msg, status))
-#define log(msg) KdPrint((DRIVER_PREFIX "%s\n", msg))
-
-#define DEREF_PTR(exp) (*(exp))
